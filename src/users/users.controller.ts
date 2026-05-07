77e117981c702b392580bb347445dd80238f7591
dweb <developersiteweb@gmail.com>
2026-05-07 16:38:41 +0200

    feat(users): add user retrieval endpoints and validation logic
    - Introduced `GET /users` to fetch all users.
    - Introduced `GET /users/:id` to fetch a user by ID, throwing `NotFoundException` if not found.
    - Added `ValidationPipe` globally to handle DTO validations.
    - Updated `createUser` method to use `CreateUserDto` and support optional `displayName`.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -7,5 +14,23 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {}
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
+  }
+
+  @Get()
+  getUsers() {
+    return this.usersService.getAllUsers();
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string) {
+    const findUser = await this.usersService.getUserById(id);
+
+    if (!findUser) {
+      throw new NotFoundException('User does not exist');
+    }
+
+    return findUser;
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,9 +8,17 @@ import { CreateUserDto } from './dto/create-user.dto';
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  createUser(createUserDto: CreateUserDto) {
+  createUser(createUserDto: CreateUserDto): Promise<User> {
     const newUser = new this.userModel(createUserDto);
 
     return newUser.save();
+  }
+
+  getAllUsers(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+
+  getUserById(id: string): Promise<User | null> {
+    return this.userModel.findById(id).exec();
   }
 }
