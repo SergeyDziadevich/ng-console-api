8f3d82423f1cd0aa2a8f0bbb7f98111d78faeef3
dweb <developersiteweb@gmail.com>
2026-05-26 17:57:04 +0200

    feat(auth): enhance sign-in functionality with SignInDto validation

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     origin: process.env.CORS_ORIGIN,
   });
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
