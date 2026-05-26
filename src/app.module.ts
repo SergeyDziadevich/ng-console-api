fdbfe08dc812d2033252ff384f132aca22024b90
dweb <developersiteweb@gmail.com>
2026-05-26 17:34:24 +0200

    feat(auth): implement JWT authentication with sign-in functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nest_mongodb'),
     UsersModule,
+    PostsModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
