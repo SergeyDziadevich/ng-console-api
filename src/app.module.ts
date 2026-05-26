4dbab48a7d39feb1121e330f5da8bbe30ea892f2
dweb <developersiteweb@gmail.com>
2026-05-26 17:46:48 +0200

    feat(config): add global configuration module for environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
@@ -6,6 +7,7 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot('mongodb://localhost/nest_mongodb'),
     UsersModule,
     PostsModule,
