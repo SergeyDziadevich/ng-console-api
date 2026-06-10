7c6c0349e1848edd59995a814d03635470acf4bc
dweb <developersiteweb@gmail.com>
2026-06-10 13:57:09 +0200

    feat(graphql): integrate GraphQL with Apollo and define user and post schemas

--- src/schemas/user-settings.schema.ts
+++ src/schemas/user-settings.schema.ts
@@ -1,13 +1,18 @@
+import { Field, ObjectType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
+@ObjectType()
 @Schema()
 export class UserSettings {
+  @Field({ nullable: true })
   @Prop({ required: false })
   receiveNotifications?: boolean;
 
+  @Field({ nullable: true })
   @Prop({ required: false })
   receiveEmails?: boolean;
 
+  @Field({ nullable: true })
   @Prop({ required: false })
   receiveSMS?: boolean;
 }
--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,32 +1,44 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose from 'mongoose';
 import { UserSettings } from './user-settings.schema';
 import { Post } from './post.schema';
 import { Role } from '../users/enums/role.enum';
 
+@ObjectType()
 @Schema()
 export class User {
+  @Field(() => ID)
+  _id: string;
+
+  @Field()
   @Prop({ unique: true, required: true })
   email: string;
 
   @Prop({ required: true })
   password: string;
 
+  @Field()
   @Prop({ unique: true, required: true })
   username: string;
 
+  @Field({ nullable: true })
   @Prop({ required: false })
   displayName?: string;
 
+  @Field({ nullable: true })
   @Prop({ required: false })
   avatarUrl?: string;
 
+  @Field(() => UserSettings, { nullable: true })
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'UserSettings' })
   settings?: UserSettings;
 
+  @Field(() => Role)
   @Prop({ type: String, enum: Role, default: Role.User })
   role: Role;
 
+  @Field(() => [Post])
   @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }] })
   posts: Post[];
 }
