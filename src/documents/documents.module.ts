e1c9194638a6bdf700e3c1c5972e78dfc39bc8fa
Sergey Dziadevich <developersiteweb@gmail.com>
2026-07-09 21:17:29 +0200

    feat(documents): Sign Documents (#34)
    * feat(documents): sign documnets
    
    * feat(documents): updated readme
    
    * feat(documents): added documents service unit tests
    
    * feat(documents): added documents service unit tests

--- src/documents/documents.module.ts
+++ src/documents/documents.module.ts
@@ -4,11 +4,14 @@ import { DocumentsController } from './documents.controller';
 import { DocumentsService } from './documents.service';
 import { Document, DocumentSchema } from '../schemas/document.schema';
 
+import { AuditModule } from '../audit/audit.module';
+
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Document.name, schema: DocumentSchema },
     ]),
+    AuditModule,
   ],
   controllers: [DocumentsController],
   providers: [DocumentsService],
