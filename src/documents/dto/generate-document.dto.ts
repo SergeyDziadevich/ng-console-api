fcd67f622b27f50dabd0d765739c4255b1a1b606
dweb <developersiteweb@gmail.com>
2026-07-10 13:39:47 +0200

    feat(documents): added msa tmp for generate document

--- src/documents/dto/generate-document.dto.ts
+++ src/documents/dto/generate-document.dto.ts
@@ -2,11 +2,12 @@ import { IsString, IsIn, IsObject, IsNotEmpty } from 'class-validator';
 
 export class GenerateDocumentDto {
   @IsString()
-  @IsIn(['invoice', 'contract', 'b2b-contract-pl'])
+  @IsIn(['msa', 'invoice', 'contract', 'b2b-contract-pl'])
   @IsNotEmpty()
-  templateType: 'invoice' | 'contract' | 'b2b-contract-pl';
+  templateType: 'msa' | 'invoice' | 'contract' | 'b2b-contract-pl';
 
   @IsObject()
   @IsNotEmpty()
   data: Record<string, any>;
 }
+
