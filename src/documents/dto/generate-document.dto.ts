781ff8e81c7daa21c38d9b433ab0ee9e855e6a89
Sergey Dziadevich <developersiteweb@gmail.com>
2026-07-10 17:41:50 +0200

    feat(documents): Generate Documents (#35)
    * feat(documents): generate document
    
    * feat(documents): generate document
    
    * feat(documents): generate document
    
    * feat(documents): added msa tmp for generate document
    
    * feat(documents): updated readme
    
    * feat(documents): lint fix

---
+++ src/documents/dto/generate-document.dto.ts
@@ -0,0 +1,12 @@
+import { IsString, IsIn, IsObject, IsNotEmpty } from 'class-validator';
+
+export class GenerateDocumentDto {
+  @IsString()
+  @IsIn(['msa', 'invoice', 'contract', 'b2b-contract-pl'])
+  @IsNotEmpty()
+  templateType: 'msa' | 'invoice' | 'contract' | 'b2b-contract-pl';
+
+  @IsObject()
+  @IsNotEmpty()
+  data: Record<string, any>;
+}
