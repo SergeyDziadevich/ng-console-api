a2f838703e4c4a392420fedd4902fb2d6b7c92e7
dweb <developersiteweb@gmail.com>
2026-07-31 14:42:56 +0200

    feat(documents): updated tests

--- src/documents/documents.service.spec.ts
+++ src/documents/documents.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { DocumentsService } from './documents.service';
 import { AiService } from '../ai/ai.service';
 import { UsersService } from '../users/users.service';
+import { EmailService } from '../email/email.service';
 import { getModelToken } from '@nestjs/mongoose';
 import { ConfigService } from '@nestjs/config';
 import { EventEmitter2 } from '@nestjs/event-emitter';
@@ -125,6 +126,10 @@ describe('DocumentsService', () => {
         {
           provide: UsersService,
           useValue: { getUserById: jest.fn() },
+        },
+        {
+          provide: EmailService,
+          useValue: { sendEmail: jest.fn() },
         },
       ],
     }).compile();
