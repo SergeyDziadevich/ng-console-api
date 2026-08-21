ce4152460b601813ab4605341ad593a34c4e0c39
dweb <developersiteweb@gmail.com>
2026-08-21 13:15:50 +0200

    feat(micro-services): migration to micro-services architecture

--- src/audit/dto/set-retention.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsInt, Min } from 'class-validator';
-import { Type } from 'class-transformer';
-
-export class SetRetentionDto {
-  @Type(() => Number)
-  @IsInt()
-  @Min(1)
-  days: number;
-}
